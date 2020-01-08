109bde495ef339c9ae9c62a8fa681750dd5d1b3a
Roderick Frizzelle <rfrizzelle@gmail.com>
2020-01-07 23:00:38 -0500

    adding controller

---
+++ src/main/java/rod/father/spring5webapp/controllers/BookController.java
@@ -0,0 +1,30 @@
+package rod.father.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import rod.father.spring5webapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+	
+	
+	private BookRepository bookRepo;
+	
+	
+	
+	
+	public BookController(BookRepository bookRepo) {
+		this.bookRepo = bookRepo;
+	}
+
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		
+		model.addAttribute("books", bookRepo.findAll());
+		
+		return "books";
+	}
+
+}
